81228123e9f6bd6dab90746546b9311ce7e8a413
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-02-07 10:49:49 -0700

    modified the effectors node to remain online after launch

--- src/effectors_node.cpp
+++ src/effectors_node.cpp
@@ -1,10 +1,14 @@
-#include <cstdio>
+#include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
+  rclcpp::init(argc, argv);
 
-  printf("hello world argus_effectors package\n");
+  auto node = rclcpp::Node::make_shared("argus_effectors");
+
+  RCLCPP_INFO(node->get_logger(), "argus_effectors online");
+
+  rclcpp::spin(node);
+  rclcpp::shutdown();
   return 0;
 }
