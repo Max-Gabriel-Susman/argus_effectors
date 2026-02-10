180ae5cb721cdf95524cfbd0fca99295464144b2
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-02-10 00:06:04 -0700

    created the effectors node class and gave it the members needed to subscribe to the inference node

--- src/effectors_node.cpp
+++ src/effectors_node.cpp
@@ -1,14 +1,33 @@
+#include <memory>
 #include <rclcpp/rclcpp.hpp>
+#include "std_msgs/msg/string.hpp"
+
+
+class EffectorsNode : public rclcpp::Node
+{
+public:
+  EffectorsNode()
+  : Node("argus_effectors")
+  {
+    sub_ = create_subscription<std_msgs::msg::String>(
+      "/argus/effectors/cmd",
+      10,
+      [this](const std_msgs::msg::String::SharedPtr msg)
+      {
+        RCLCPP_INFO(get_logger(), "received cmd: %s", msg->data.c_str());
+      });
+
+    RCLCPP_INFO(get_logger(), "argus_effectors online");
+  }
+
+private:
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_;
+};
 
 int main(int argc, char ** argv)
 {
   rclcpp::init(argc, argv);
-
-  auto node = rclcpp::Node::make_shared("argus_effectors");
-
-  RCLCPP_INFO(node->get_logger(), "argus_effectors online");
-
-  rclcpp::spin(node);
+  rclcpp::spin(std::make_shared<EffectorsNode>());
   rclcpp::shutdown();
   return 0;
 }
